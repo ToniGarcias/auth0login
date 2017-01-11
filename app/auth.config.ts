37f872bd9e745ed6d3b626a1e9b04bb47142a305
ToniGarcias <toni.garcias.mas@gmail.com>
2017-01-11 18:25:19 +0100

    ok

---
+++ app/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'lRfS7MRJV3C6ForcQldeIOUp1VQEFJdb',
+    domain: 'tonigarcias.eu.auth0.com'
+};
